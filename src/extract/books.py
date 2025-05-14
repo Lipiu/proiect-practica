131a27b0c30189da97ce0012f31352835dc9c3bf
Rosu Liviu <rosuliviu10@gmail.com>
2025-05-14 16:09:45 +0300

    added books table but publisher is null, will have to fix that

---
+++ src/extract/books.py
@@ -0,0 +1,69 @@
+import requests
+import psycopg2
+import csv
+
+def extract_data(file_path: str, limit: 500): #because there are 200_000 rows and it takes too long to load
+    """
+    Extract data from the csv file.
+    """
+    data = []
+    with open(file_path, mode='r') as file:
+        csv_reader = csv.DictReader(file, delimiter = ';')
+        for i, row in enumerate(csv_reader):
+            if i >= limit:
+                break
+            data.append(row)
+    return data
+
+def parse_data(row: dict):
+    """
+    Parsing data
+    """
+
+    data = {
+    #titlu, autor, year, publisher
+        "title": row["Book-Title"],
+        "author": row["Book-Author"],
+        "year_of_publication": row["Year-Of-Publication"],
+        "publisher:": row["Publisher"]
+    }
+
+    return data
+
+def load_data(data: dict) -> None:
+    conn = psycopg2.connect(
+        dbname = "talesTomes",
+        user = "postgres",
+        password = "2805",
+        host = "localhost",
+        port = 5432
+    )
+
+    cursor = conn.cursor()
+
+    sql = """
+        INSERT INTO raw.books (
+            title, author, year_of_publication, publisher
+        ) VALUES (
+            %s, %s, %s, %s
+);
+    """
+
+    cursor.execute(
+        sql,
+        (
+            data.get("title"),
+            data.get("author"),
+            data.get("year_of_publication"),
+            data.get("publisher"),
+        )
+    )
+    conn.commit()
+    cursor.close()
+    conn.close()
+
+if __name__ == "__main__":
+    csv_data = extract_data('data/books.csv', limit=500)
+    for row in csv_data:
+        parsed_data = parse_data(row)
+        load_data(parsed_data)
---
+++ src/tables raw/create_books.sql
@@ -0,0 +1,6 @@
+CREATE TABLE IF NOT EXISTS raw.books(
+    title VARCHAR(300),
+    author VARCHAR(300),
+    year_of_publication TEXT,
+    publisher VARCHAR(300)
+)
